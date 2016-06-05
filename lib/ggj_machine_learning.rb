4ffe062661a6f1eb783a654955470ed7e3d6623d
Guillermo Garcia Juanes <ggjuanes@gmail.com>
2016-06-05 19:53:41 +0100

    Add methods with TODO to Machine Learning class

--- lib/ggj_machine_learning.rb
+++ lib/ggj_machine_learning.rb
@@ -1,19 +1,31 @@
 require "ggj_machine_learning/version"
+require "ggj_machine_learning/models/linear_regression"
 
 class GgjMachineLearning
+  @model = nil
+  @points = []
+
+  def initialize
+    @points = []
+  end
+
   def self.helloWorld
     "Hello World!"
   end
 
   def setModel
-    # @TODO
+    @model = LinearRegression.new
   end
 
-  def learn
-    # @TODO
+  def learn(points)
+    @points += points
+
+    @model.set_data(@points)
+
+    @model.gradient_descent_method
   end
 
-  def predict
-    # @TODO
+  def predict(x)
+    @model.h(x)
   end
 end
--- spec/ggj_machine_learning_spec.rb
+++ spec/ggj_machine_learning_spec.rb
@@ -8,4 +8,39 @@ describe GgjMachineLearning do
   it 'Hello World' do
     expect(GgjMachineLearning.helloWorld).to eq "Hello World!"
   end
+
+  it 'Set Model' do
+    m_learning = GgjMachineLearning.new
+
+    m_learning.setModel
+
+    model = m_learning.instance_variable_get(:@model)
+
+    expect(model.class).to be(LinearRegression)
+  end
+
+  it 'Learn - Add more points' do
+    m_learning = GgjMachineLearning.new
+    m_learning.setModel
+
+    m_learning.instance_variable_get(:@model).stub(:gradient_descent_method) do
+      1
+    end
+
+    points1 = [
+        [0,0],
+        [1,1]
+    ]
+    m_learning.learn(points1)
+
+    points2 = [
+        [2,2],
+        [3,3]
+    ]
+    m_learning.learn(points2)
+
+    m_points = m_learning.instance_variable_get(:@points)
+
+    expect(m_points.length).to be 4
+  end
 end
