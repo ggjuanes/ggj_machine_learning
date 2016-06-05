c2cb244768c8dc0e0a43822af008ae576c591ef7
Guillermo Garcia Juanes <ggjuanes@gmail.com>
2016-06-05 10:18:10 +0100

    Add skeleton for Machine Learning class

--- lib/ggj_machine_learning.rb
+++ lib/ggj_machine_learning.rb
@@ -4,4 +4,16 @@ class GgjMachineLearning
   def self.helloWorld
     "Hello World!"
   end
+
+  def setModel
+    # @TODO
+  end
+
+  def learn
+    # @TODO
+  end
+
+  def predict
+    # @TODO
+  end
 end
